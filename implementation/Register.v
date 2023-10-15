0a73eb42cb9b5d2a3fbe676a83341fa183ca8620
ferrerjulian <ferrerj@rose-hulman.edu>
2023-10-15 17:41:14 -0400

    Register implementation

---
+++ implementation/Register.v
@@ -0,0 +1,16 @@
+module register(
+    input [15:0] reg_input,
+    input reset[0:0],
+    input CLK [0:0],
+    output reg [15:0] reg_output
+)
+
+always @ (posedge(CLK))
+begin
+    if(reset != 1) begin
+        reg_output = reg_input;
+    end
+        reg_output = reg_input;
+end
+
+endmodule
