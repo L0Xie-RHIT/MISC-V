092e36c7611f182c95e2f20323550366a6443f5c
ferrerjulian <ferrerj@rose-hulman.edu>
2023-10-30 15:55:29 -0400

    Write stage tests and fix

--- implementation/Write_Stage.v
+++ implementation/Write_Stage.v
@@ -6,14 +6,15 @@ module Write_Stage(
     input [15:0] ALUResult,
     input [15:0] StoreMem,
     input [2:0] rdWB,
-    output reg [15:0] loadData,
-    output reg [2:0] loadAddr,
-    output reg regWriteOut
+    output [15:0] loadData,
+    output [2:0] loadAddr,
+    output [0:0] regWriteOut
 );
 
-wire reg [15:0] StoreMemCon;
-wire reg [15:0] ALUResultCon;
-wire reg [0:0] RegStoreCon;
+wire [15:0] StoreMemCon;
+wire [15:0] ALUResultCon;
+wire [0:0] RegStoreCon;
+
 
 MEM_WB MEMWBRB(
     .IRegWrite(RegWrite),
