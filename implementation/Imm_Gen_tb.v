848f88296974be056d7483ad2f3ca390ca99f533
L0Xie-RHIT <xiez6@rose-hulman.edu>
2023-11-01 16:55:09 -0400

    Not tested but should work

--- implementation/Imm_Gen_tb.v
+++ implementation/Imm_Gen_tb.v
@@ -15,76 +15,66 @@ module Imm_Gen_tb;
   );
 
   initial begin
-    // Test type R
-    instruction = 16'b0000000000000001;
-    #10;
-    if (immediate !== 16'b0000000000000001) $display("Type R positive case failed");
-    #10;
-    instruction = 16'b0000000000001000;
-    #10;
-    if (immediate !== 16'b1111111111111000) $display("Type R negative case failed");
-    #10;
-
     // Test type I
-    instruction = 16'b0010000000000100;
+    instruction = 16'b00000010000000001;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type I positive case failed");
     #10;
-    instruction = 16'b0010000000100000;
+    instruction = 16'b00100000000000001;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type I negative case failed");
     #10;
 
     // Test type M
-    instruction = 16'b0100000000000001;
+    instruction = 16'b0000001000000010;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type M positive case 1 failed");
     #10;
-    instruction = 16'b0100000001000000;
+    instruction = 16'b1000000000000010;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type M negative case 1 failed");
     #10;
-    instruction = 16'b0110000000000001;
+    instruction = 16'b0000001000000011;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type M positive case 2 failed");
     #10;
-    instruction = 16'b0110000001000000;
+    instruction = 16'b1000000000000011;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type M negative case 2 failed");
     #10;
 
     // Test type Y
-    instruction = 16'b1000000000000001;
+    instruction = 16'b0000000000001100;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type Y positive case 1 failed");
     #10;
-    instruction = 16'b1001000000000000;
+    instruction = 16'b1000000000000100;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type Y negative case 1 failed");
     #10;
-    instruction = 16'b1010000000000001;
+    instruction = 16'b0000000000001101;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type Y positive case 2 failed");
     #10;
-    instruction = 16'b1011000000000000;
+    instruction = 16'b1000000000000101;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type Y negative case 2 failed");
     #10;
 
     // Test type J
-    instruction = 16'b1100000000000001;
+    instruction = 16'b0000000001000110;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type J positive case 1 failed");
     #10;
-    instruction = 16'b1101000000000000;
+    instruction = 16'b1000000000000110;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type J negative case 1 failed");
     #10;
-    instruction = 16'b1110000000000001;
+    instruction = 16'b0000000000001000111;
     #10;
     if (immediate !== 16'b0000000000000001) $display("Type J positive case 2 failed");
     #10;
-    instruction = 16'b1111000000000000;
+    instruction = 16'b0001000000000000111;
     #10;
     if (immediate !== 16'b1111111111111000) $display("Type J negative case 2 failed");
     #10;
